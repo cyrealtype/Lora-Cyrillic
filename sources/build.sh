0fe4e4cac609799e10a1e8bf6e395c47c1b36a72
alexeiva <a+mygit@cyreal.org>
2019-02-03 22:18:23 +0700

    cleaned up kerning. build script now generates TTF of Italic. Roman fails

--- sources/build.sh
+++ sources/build.sh
@@ -3,7 +3,7 @@ set -e
 
 
 echo "Generating Static fonts"
-mkdir -p ../fonts
+#mkdir -p ../fonts
 fontmake -g Lora.glyphs -i -o ttf --output-dir ../fonts/TTF/
 fontmake -g Lora-Italic.glyphs -i -o ttf --output-dir ../fonts/TTF/
 fontmake -g Lora.glyphs -i -a -o otf --output-dir ../fonts/OTF/
