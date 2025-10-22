990d096770ec849579270d3031e1b84ea32e1272
Alexei Vanyashin <a+mygit@cyreal.org>
2025-10-22 17:48:31 +0500

    generated fonts v.3.021

--- build.sh
+++ build.sh
@@ -3,7 +3,5 @@ set -e
 
 echo "Generating fonts"
 gftools builder ./sources/config.yaml
-echo "Compressing woffs"
-for FILE in ./fonts/variable/*.ttf; do python3 -m fontTools ttLib.woff2 compress $FILE; done
-
-
+#echo "Compressing woffs"
+#for FILE in ./fonts/variable/*.ttf; do python3 -m fontTools ttLib.woff2 compress $FILE; done
